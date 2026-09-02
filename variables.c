5d8c6abb328ccef07977fdf6384162a007b33728
khushi shrivastav <kshrivastav2368@gmail.com>
2026-09-02 17:25:05 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age=18;
+float marks=85.5;
+char grade='A';
+
+printf("age=%d\n",age);
+printf("marks=%.1f\n",marks);
+printf("grade=%c\n",grade);
+
+return 0;
+}
